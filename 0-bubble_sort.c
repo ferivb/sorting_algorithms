6b530e5bc7d059e68c3ec4584d0282f231fccaf5
Felipe Rivas <fe.rivasb@gmail.com>
2022-02-13 11:57:37 -0500

    task 0 attempt 1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * unsorted - Checks if the array is sorted
+ *
+ * @array: The array to check
+ * @size: The size of the array
+ * Return: 1 if unsorted, 0 if sorted
+ */
+int unsorted(int *array, size_t size)
+{
+	size_t i = 0;
+
+	for (; i < (size - 1); i++)
+	{
+		if (array[i] > array[i + 1])
+			return (1);
+	}
+	return (0);
+}
+
+
+/**
+ * bubble_sort - Sorts 2 positions each iteration
+ *
+ * @array: Array to sort
+ * @size: Size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp = 0;
+	size_t j = 0;
+
+	while (unsorted(array, size))
+	{
+		j = 0;
+		for (; j < (size - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array((const int *)array, size);
+			}
+		}
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * @brief 
+ * 
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t **temp;
+	int i, j;
+
+	if (list->next->n > list->n)
+	{
+		
+	}
+}
