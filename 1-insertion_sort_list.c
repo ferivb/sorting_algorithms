80b20f374c373c8b58b648ba42734bb2a273b0ba
Felipe Rivas <fe.rivasb@gmail.com>
2022-02-15 23:10:20 -0500

    task 1 attempt 9000 seg fault on last node

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,16 +1,46 @@
 #include "sort.h"
+#include <unistd.h>
 
 /**
- * @brief 
- * 
+ * insertion_sort_list - Sorts a DLL
+ *
+ * @list: list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t **temp;
-	int i, j;
+	listint_t *current = NULL, *temp = NULL;
 
-	if (list->next->n > list->n)
+	current = *list;
+	while (current != NULL)
 	{
-		
+		if (current->n > current->next->n)
+		{
+			current = current->next;
+			while (current->prev != NULL)
+			{
+				if (current->n < current->prev->n)
+				{
+					temp = current->next;
+					current->next = current->prev;
+					current->prev = current->prev->prev;
+					if (current->prev != NULL)
+						current->prev->next = current;
+					else
+						*list = current;
+					current->next->prev = current;
+					if (temp != NULL)
+					{
+						current->next->next = temp;
+						temp->prev = current->next;
+					}
+					else
+						current->next->next = NULL;
+					print_list(*list);
+				}
+				else
+					current = current->prev;
+			}
+		}
+		current = current->next;
 	}
 }
